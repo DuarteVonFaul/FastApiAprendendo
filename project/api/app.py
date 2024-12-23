2498f5c734977817a512b48a3921a8227cfd9b4c
DuarteVonFaul <duarteriograndense@gmail.com>
2024-12-23 15:03:18 -0300

    FEAT[RegisterUser] Create the UserScheme (for receive) and UserPublic(For return) schemes, and create an endpoint for users to register

--- project/api/app.py
+++ project/api/app.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
 from http import HTTPStatus
-from api.schemas import Message
+from api.schemas import Message, UserSChema, UserPublic
+
 app = FastAPI()
 
 
-@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
+@app.get('/', 
+         status_code=HTTPStatus.OK, 
+         response_model=Message)
 def read_root(): 
     return {'message': 'Hello World'}
+
+
+@app.post('/users/', 
+          status_code=HTTPStatus.CREATED, 
+          response_model= UserPublic)
+def create_user(user:UserSChema):
+    return user
+
+
+
--- project/api/schemas.py
+++ project/api/schemas.py
@@ -1,5 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class Message(BaseModel):
     message: str
+
+
+class UserSChema(BaseModel):
+    username:str
+    password:str
+    email:EmailStr
+
+class UserPublic(BaseModel):
+    username:str
+    email:EmailStr
