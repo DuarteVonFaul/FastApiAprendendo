b52371b823aabd606facffb2799bffc618552994
DuarteVonFaul <duarteriograndense@gmail.com>
2024-12-27 16:25:58 -0300

    TEST[Adding user update test] Creating a user update test to validate the updating of user information

--- project/tests/test_app.py
+++ project/tests/test_app.py
@@ -36,3 +36,22 @@ def test_read_users(client):
         }]
     }
     ...
+
+
+def test_update_user(client):
+
+    response = client.put('/users/1',
+        json={
+            'username': 'testeuser',
+            'password': 'pass123',
+            'email': 'user@exemple.com',
+        },)
+
+    assert response.status_code == HTTPStatus.OK
+
+    assert response.json() == {
+        'username': 'testeuser',
+        'email': 'user@exemple.com',
+        'id': 1
+        }
+    ...
