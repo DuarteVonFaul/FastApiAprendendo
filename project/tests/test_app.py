e0f2187f022d1d972ef9aa8c53b7536248df65ce
DuarteVonFaul <duarteriograndense@gmail.com>
2024-12-27 13:30:51 -0300

    Refactor[refactoring the use of the Client] Now, use of the client is linked to the client method and the client parameter  in the test method

--- project/tests/test_app.py
+++ project/tests/test_app.py
@@ -1,11 +1,15 @@
 from http import HTTPStatus
-
+import pytest
 from fastapi.testclient import TestClient
 from api.app import app
 
+#Sempre que eu tiver um metodo de teste que recebe um client
+# ele recebera essa função como parametro
+@pytest.fixture()
+def client():
+    return TestClient(app)
 
-def test_create_user():
-    client = TestClient(app)
+def test_create_user(client):
 
     # UserSchema
     response = client.post(
