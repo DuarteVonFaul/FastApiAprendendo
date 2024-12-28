5db3cde1b8de45429963010f8f5a6b93e73c967e
DuarteVonFaul <duarteriograndense@gmail.com>
2024-12-28 11:25:24 -0300

    FEAT[USER NOT FOUND ERROR]Creating validate for existing users and returning USER NOT FOUND when the user does not exist

--- project/api/app.py
+++ project/api/app.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from api.schemas import Message, UserDB, UserList, UserPublic, UserSChema
 
@@ -30,7 +30,14 @@ def read_users():
 
 @app.put('/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
 def update_user(user_id:int, user: UserSChema):
+
+    if(user_id < 1 or user_id > len(database)):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='USER NOT FOUND'
+            )
+    
     user_update = UserDB(id=user_id, **user.model_dump())
     database[user_id - 1] = user_update
+        
 
     return user_update
